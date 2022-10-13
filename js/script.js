5de11165d33e72e655ac8a313dbe866d629e6ef5
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-13 15:33:53 -0400

    code change

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Created by: Mikey Gloriani
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets address inputed by a user and shows it back to user.
+ */
+ function enterClicked() {
+    // input
+    const streetName = document.getElementById("street-name").value
+    const streetNumber = parseInt(document.getElementById("street-number").value)
+  
+    // output
+    document.getElementById("address").innerHTML =
+      "Your address is: " + streetNumber + streetName  + "."
+  }
